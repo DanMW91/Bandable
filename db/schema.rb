43d9d720dc931b2f03fceb37b1b81e3169d95672
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-23 13:17:40 +0000

    hm

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2021_11_22_175744) do
-
+ActiveRecord::Schema.define(version: 2021_11_23_130209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_175744) do
     t.string "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -62,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_175744) do
     t.string "last_name"
     t.string "location"
     t.string "bio"
+    t.string "genre"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
