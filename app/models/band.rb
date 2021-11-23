1b09a992dedaa464c59ca6371b6f6a8ac5dea25c
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-23 14:34:25 +0000

    seeds and validation

--- app/models/band.rb
+++ app/models/band.rb
@@ -4,9 +4,10 @@ class Band < ApplicationRecord
   # has_many_attached :photos
 
   validates :bio, length: { maximum: 1000 }
-  validates :name, :user_id, :location, :bio, :current_member_count, presence: true
+  validates :name, :location, :bio, :current_member_count, presence: true
 
   has_many :messages
+  has_many :band_members
   has_many :users, through: :band_members
 
 end
