84302bcef3f15b9111c1d36dac178423e9d97288
unelson <theartist@live.co.uk>
2021-11-23 12:45:20 +0000

    added validations and associations to user and band models

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,7 @@
 class Band < ApplicationRecord
+  # has_many :users, dependent: :destroy
+  # has_many_attached :photos
+
+  validates :bio, length: { maximum: 1000 }
+  validates :name, :user_id, :location, :bio, :current_member_count, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # user has the ability to join many bands and only has the ability to upload
+  # one photo - the avatar
+  # has_one_attached :photo
+  has_many :instruments, through: :user_instruments
+  has_many :genres, through: :user_genres
+  validates :first_name, :last_name, :location, :bio, presence: true
+  validates :email, confirmation: true, uniqueness: true
+  validates :first_name, :last_name, length: { maximum: 16 }
+  validates :bio, length: { maximum: 1000 }
+  validates :instruments, inclusion: { in: %w[Guitar Drums Piano Bass Vocals Violin Cello Flute] }
+  validates :genres, inclusion: { in:
+    ['Rock', 'Jazz', 'RnB', 'Hip-Hop', 'Heavy Metal', 'Country', 'Folk', 'Pop', 'Indie'] }
 end
