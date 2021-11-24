5e51798b8117f90799dba3cd069a7c63cb09e7d6
unelson <theartist@live.co.uk>
2021-11-24 17:14:56 +0000

    spotify authentication. Incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'band_members/create'
   get 'bands/index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'bands#index'
   resources :bands do
     resources :band_members, only: [:index]
