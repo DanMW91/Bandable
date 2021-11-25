1dcec130ac4d39566169f541c69ee30c8c7790db
kenneth campbell <kencam92@gmail.com>
2021-11-25 17:28:29 +0000

    ran db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2021_11_25_152746) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,8 +64,6 @@ ActiveRecord::Schema.define(version: 2021_11_25_152746) do
     t.string "bio"
     t.string "genre"
     t.string "avatar_url"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
