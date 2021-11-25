4356c9a2eb1a952287e0632e6e0d88194d046284
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-11-25 15:29:32 +0000

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_171100) do
+ActiveRecord::Schema.define(version: 2021_11_25_152746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_171100) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "genre"
     t.string "image_url"
+    t.string "spotify_embed_url"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -63,8 +64,6 @@ ActiveRecord::Schema.define(version: 2021_11_24_171100) do
     t.string "bio"
     t.string "genre"
     t.string "avatar_url"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
