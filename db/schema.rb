fdc4c2f9508a0c04d1476a074dce9bcf45f767d6
unelson <theartist@live.co.uk>
2021-11-25 16:57:51 +0000

    git pulling

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2021_11_25_152746) do
 
   # These are extensions that must be enabled in order to support this database
