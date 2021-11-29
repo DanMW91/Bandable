1a1e36e8bfc308d491df8fba3408393f63bd05c4
kenneth campbell <kencam92@gmail.com>
2021-11-29 15:43:06 +0000

    changed permit(:power) to permit(:band) in controller

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -12,6 +12,6 @@ class BandsController < ApplicationController
   private
 
   def band_params
-    params.require(:power).permit(:photo, :name, :current_member_count, :location, :bio, :genre, :photo)
+    params.require(:band).permit(:photo, :name, :current_member_count, :location, :bio, :genre, :photo)
   end
 end
