40d23f3589381d37a373deacbfe5c190cb9a165e
unelson <theartist@live.co.uk>
2021-11-30 10:05:16 +0000

    ajax search abandoned. Other small fixes and changes relating to search made

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,9 +1,10 @@
 class BandsController < ApplicationController
   def index
-    @bands = Band.order(name: :genre)
-    if params[:query].present?
-      @bands = @bands.where('title ILIKE ?', "%#{params[:query]}%")
-    end
+    # Failed Ajax attempt
+    # @bands = Band.order(name: :asc, genre: :asc)
+    # if params[:query].present?
+    #   @bands = @bands.where('title ILIKE ?', "%#{params[:query]}%")
+    # end
 
     if params[:query].present?
       @bands = Band.global_search(params[:query])
@@ -11,20 +12,11 @@ class BandsController < ApplicationController
       @bands = Band.all
     end
 
-    respond_to do |format|
-      format.html
-      format.text { render partial: 'list.html', locals: { bands: @bands } }
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.text { render partial: 'list.html', locals: { bands: @bands } }
+    # end
 
-  update() {
-    const url = `${this.formTarget.action}?
-  query=${this.searchInputTarget.value}`
-    fetch(url, { headers: { 'Accept': 'text/plain' } })
-      .then(response => response.text())
-      .then((data) => {
-        this.listTarget.outerHTML = data;
-      })
-  }
   end
 
   def show
@@ -68,6 +60,6 @@ class BandsController < ApplicationController
   private
 
   def band_params
-    params.require(:band).permit(:photo, :name, :current_member_count, :location, :bio, :genre, :photo)
+    params.require(:band).permit(:name, :current_member_count, :location, :bio, :genre)
   end
 end
