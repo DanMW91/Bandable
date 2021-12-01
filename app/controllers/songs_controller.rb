3a6fdb32aff92ab9b201fb63915233c118832ec8
Daniel Maile-Wilson <danny.mw1991@gmail.com>
2021-12-01 17:16:57 +0000

    song controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def create
     new_song = Song.new(song_params)
     new_song.band_id = params[:band_id]
-    new_song.user = current_user
+    new_song.user_id = current_user.id
     if new_song.save
       redirect_to band_path(params[:band_id], anchor: "the%20rehearsal%20room")
     else
